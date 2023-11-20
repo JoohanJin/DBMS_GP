34e59ae1b26c1d7c7167c8091896f6c0dbf69718
JoohanJin <wngks3146@naver.com>
2023-11-20 20:34:41 +0800

    added assets, images  and made adjustment for the db sql and editting main.py currently

--- DB.sql
+++ DB.sql
@@ -1,29 +1,40 @@
 CREATE TABLE Student (
-    student_id int NOT NULL,
+    student_id varchar(10) NOT NULL,
     email varchar(60) NOT NULL,
-    password varchar(80) NOT NULL,
-    name varchar(80) NOT NULL,
-    login_time time NOT NULL,
-    login_date date NOT NULL
+    pwd varchar(80) NOT NULL,
+    username varchar(80) NOT NULL,
+    /* login_time time NOT NULL,
+    login_date date NOT NULL */     /* 여기는 일단 student values 말고 다른 로그인 테이블에 넣어야 할거 같아서 보류해놨음 */
+    /* 굳 */
 );
+
+/* student information inserting into student values */
+INSERT INTO Student VALUES 
+    ('3035678110', "kyoungmin2@connect.hku.hk", "qkrrudals123!@", "Jamie",),
+    ('3035678111', "dayekim1@connect.hku.hk", "rlaekdP12!@", "Diana",),
+    ('3035678112', "seunghoon2@connect.hku.hk", "dltmdgns123!@", "Kevin",),
+    ('3035678113', "hosungkang3@connect.hku.hk", "rkdghtjd12!@", "Harvey",),
+    ('3035678114', "dongjunyeom1@connect.hku.hk", "duaehdwns123!@","Andy",),
+    ('3035678115', "jihoo2@connect.hku.hk", "rlaekdP12!@", "Jim",),
+    ('3035678116', "soheehee@connect.hku.hk", "dlathgml12!@","Helena",),
+    ('3035678117', "yuhyun2@connect.hku.hk", "dldbgus123!@", "Mindy",),
+    ('3035661360', "wngks873@connect.hku.hk", "1q2w3e4r!!", "joe",);
 
 CREATE TABLE Course (
     course_id int NOT NULL,
     course_name varchar(80) NOT NULL,
     teacher_message text NOT NULL,
-    lecture_notes text NOT NULL,
+    tutorial_notes text NOT NULL,
     course_website_link varchar(120) NOT NULL,
-    checked BOOLEAN.
-    lecture_time time NOT NULL,
-    which_day int NOT NULL
+    checked BOOLEAN,
+    course_start_time time NOT NULL
+    course_end_time time NOT NULL /* added course start and end time here, deleted from the 'LogIn_Time' table below */
 );
 
 CREATE TABLE LogIn_Time (
+    student_id varchar(10) NOT NULL,
     login_date date NOT NULL,
-    start_time time NOT NULL,
-    end_time time NOT NULL,
-    current_day varchar(20) NOT NULL,
-    course_id int NOT NULL
+    login_time time NOT NULL
 );
 
 CREATE TABLE Record (
@@ -38,22 +49,14 @@ CREATE TABLE TimeTable (
     time_table_image LONGBLOB NOT NULL
 );
 
-INSERT INTO Student VALUES (
-    1,
-    "wngks873@connect.hku.hk",
-    "1q2w3e4r!!",
-    "joe",
-    "00:00:00",
-    "2023-11-14"
-);
+/* student timetable information inserting in with images */
 
-INSERT INTO Course VALUES (
-    "COMP3278",
-    "INTRO To DBMS",
-    "this is a test announcement",
-    "https://moodle.hku.hk/mod/resource/view.php?id=3081895",
-    "https://moodle.hku.hk/course/view.php?id=106523",
-    1,
-    "14:30:00,15:30:00",
-    12
-);
+INSERT INTO TimeTable VALUES 
+    (1, 3035678110, LOAD_FILE('/project_DB/timetable_1.png'),),
+    (2, 3035678111, LOAD_FILE('/project_DB/timetable_2.png'),),
+    (3, 3035678112, LOAD_FILE('/project_DB/timetable_3.png'),),
+    (4, 3035678113, LOAD_FILE('/project_DB/timetable_4.png'),),
+    (5, 3035678114, LOAD_FILE('/project_DB/timetable_5.png'),),
+    (6, 3035678115, LOAD_FILE('/project_DB/timetable_6.png'),),
+    (7, 3035678116, LOAD_FILE('/project_DB/timetable_7.png'),),
+    (8, 3035678117, LOAD_FILE('/project_DB/timetable_8.png'),);
--- main.py
+++ main.py
@@ -3,17 +3,155 @@ import cv2
 import mysql.connector
 import PySimpleGUI as sg
 import pickle
-from datetime import datetime
+import datetime
 import login_page
 import sys, res
 import PySimpleGUI as sg
+import time
+
+# GLOBAL VARIABLE
+# DB connection 
+db_connection = mysql.connector.connect(host="localhost", user="joe", passwd="1Q2w#E$R!!", database="project")
+cursor = db_connection.cursor()
+
+
+def auto_login():
+    global current_time
+    global date_stamp
+    global time_stamp
+    gui_confidence = 60
+    camera_time_period = 10
+    recognized = 0
+
+    current_time = time.time()
+    camera_limit = current_time + camera_time_period
+
+    date_stamp = datetime.datetime.fromtimestamp(time_stamp).strftime("%Y-%m-%d")
+    time_stamp = datetime.datetime.fromtimestamp(time_stamp).strftime("%H:%M:%S")
+    
+    labels = {"person_name": 1}
+    with open("labels.pickle", "rb") as f:
+        labels = pickle.load(f)
+        labels = {v: k for k, v in labels.items()}
+
+    if time.time() < camera_limit:
+        recognizer = cv2.face.LBPHFaceRecognizer_create()
+        try:
+            recognizer.read("train.ymal")
+        except:
+            error_msg = "Face Recognition Model has been corrupted or could be found!"
+            # display msg?
+
+        face_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
+        cap = cv2.VideoCapture(0)
+        
+        while True:
+            ret, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+            for (x, y, w, h) in faces:
+                roi_gray = gray[y:y+h, x:x+ w]
+                roi_color = frame[y:y+h, x:x+w]
+                id_, conf = recognizer.predict(roi_gray)
+
+                if conf >= gui_confidence:
+                    fonr = cv2.QT_FONT_NORMAL
+                    if id_ == 0:
+                        id = "3035661360"
+                    current_id = id
+                    color = (255, 0, 0)
+                    stroke = 2
+                    cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), (2))
+                    recognized = 1
+                # If the face is unrecognized
+                else:
+                    color = (255, 0, 0)
+                    stroke = 2
+                    font = cv2.QT_FONT_NORMAL
+                    cv2.putText(frame, "UNKNOWN", (x, y), font, 1, color, stroke, cv2.LINE_AA)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), (2))
+                    hello = ("Your face is not recognized")
+                    print(hello)
+                    # engine.say(hello)
+                    # engine.runAndWait()
+            
+            if time.time() > camera_limit:
+                break
+
+            cv2.imshow('Checking your face id', frame)
+            key_wait = cv2.waitKey(10) & 0xff
+            if key_wait == 27:
+                break
+
+        cap.release()
+        cv2.destoryAllWindows()
+
+        if (recognized): # if the user face id has been recognized successfully
+            # update the database with the new login time finding the student id
+            query = "SELECT student_id, name, login_date, login_time, login_date FROM Student WHERE name='%s'" % (current_id)
+            select = cursor.execute(query)
+            result = cursor.fetchall()
+            print(result)
+            data = "error"
+
+        for x in result:
+            data = x
+
+        # If the student's information is not found in the database
+        if data == "error":
+            # the student's data is not in the database
+            print("The student", current_id, "is NOT FOUND in the database.")
+
+        # If the student's information is found in the database
+        else:
+            update =  "UPDATE Student SET login_date=%s WHERE name=%s"
+            # update = "UPDATE test SET date_of_join=%s WHERE name=%s"
+            val = (date_stamp, current_id)
+            cursor.execute(update, val)
+            update = "UPDATE Student SET login_time=%s WHERE name=%s"
+            # update = "UPDATE test SET date_of_join=%s WHERE name=%s"
+            val = (current_time, current_id)
+            cursor.execute(update, val)
+            db_connection.commit()
+
+            welcome_message = f""
+            # hello = ("Hello ", current_name, "You did attendance today")
+            # print(hello)
+            # engine.say(hello)
+
+
+        # GUI
+        imgbytes = cv2.imencode('.png', frame)[1].tobytes() 
+        if not win_started:
+            win_started = True
+            layout = [
+                [sg.Text('Attendance System Interface', size=(30,1))],
+                [sg.Image(data=imgbytes, key='_IMAGE_')],
+            ]
+            win = sg.Window('Attendance System',
+                    default_element_size=(14, 1),
+                    text_justification='right',
+                    auto_size_text=False).Layout(layout).Finalize()
+            image_elem = win.FindElement('_IMAGE_')
+        else:
+            image_elem.Update(data=imgbytes)
+
+        event, values = win.Read(timeout=20)
+        if event is None or event == 'Exit':
+            break
+
+    
+    return
+
+def manual_login():
+    return
 
 def face_rec():
     gui_confidence = 60
 
     db_connection = mysql.connector.connect(host="localhost", user="joe", passwd="1Q2w#E$R!!", database="project")
-    date = datetime.utcnow()
-    now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     cursor = db_connection.cursor()
 
